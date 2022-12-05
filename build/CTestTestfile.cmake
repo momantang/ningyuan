e60bc834bad7b40953af22682976e541425cb6c0
momantang <momantang@163.com>
2022-12-06 01:57:38 +0800

    del build dir
    del build1

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/ningyuan
-# Build directory: C:/dev/ningyuan/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
-subdirs("app")
--- build/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/ningyuan/app
-# Build directory: C:/dev/ningyuan/build/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
